7ba9cc7f611eaeeba963f901055e32061656e3bd
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-04 17:31:28 -0300

    feat: add mapper for DTOs

---
+++ Ping-API.Application/Mappers/ReminderMapper.cs
@@ -0,0 +1,27 @@
+﻿using Ping_API.Application.DTOs;
+using PingAPI.Domain.Entities;
+
+namespace Ping_API.Application.Mappers
+{
+    public static class ReminderMapper
+    {
+        public static ReminderResponseDto ToDto(Reminder reminder) => new(
+            reminder.Id,
+            reminder.Title,
+            reminder.Description,
+            reminder.NotificationDate,
+            reminder.AdvanceNotice,
+            reminder.Link,
+            reminder.CreatedAt
+            );
+
+        public static Reminder ToEntity(CreateReminderDto dto) => new(
+            dto.Title,
+            dto.Description,
+            dto.NotificationDate,
+            dto.AdvanceNotice,
+            dto.Link,
+            DateTime.UtcNow
+            );
+    }
+}
