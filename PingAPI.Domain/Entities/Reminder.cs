7f5a57bfec1b26b6ce7464d899f5e5f9237aa6ea
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-04 12:06:48 -0300

    feat: add Reminder domain entity

---
+++ PingAPI.Domain/Entities/Reminder.cs
@@ -0,0 +1,39 @@
+﻿namespace PingAPI.Domain.Entities
+{
+    public class Reminder
+    {
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime NotificationDate { get; private set; }
+        public int AdvanceNotice { get; private set; }
+        public string? Link { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+
+        public Reminder(string title, string description, DateTime notificationDate, int advanceNotice, string? link, DateTime createdAt)
+        {
+            Title = title;
+            Description = description;
+            NotificationDate = notificationDate;
+            AdvanceNotice = advanceNotice;
+            Link = link;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        protected Reminder() { }
+
+        public void Update(string title, string description, DateTime notificationDate, int advanceNotice, string? link)
+        {
+            Title = title;
+            Description = description;
+            NotificationDate = notificationDate;
+            AdvanceNotice = advanceNotice;
+            Link = link;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+
+    }
+}
---
+++ PingAPI.Domain/Interfaces/IReminderRepository.cs
@@ -0,0 +1,13 @@
+﻿using PingAPI.Domain.Entities;
+
+namespace PingAPI.Domain.Interfaces
+{
+    public interface IReminderRepository
+    {
+        Task<Reminder?> GetByIdAsync(int id);
+        Task<IEnumerable<Reminder>> GetAllAsync();
+        Task AddAsync(Reminder reminder);
+        Task UpdateAsync(Reminder reminder);
+        Task DeleteAsync(int id);
+    }
+}
