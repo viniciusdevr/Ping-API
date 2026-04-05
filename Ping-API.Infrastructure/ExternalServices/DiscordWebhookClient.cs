1e779f4fba2e42975596d84e3a73f1c97e62a558
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-05 18:42:35 -0300

    feat: added Dicord Webhook implementation

---
+++ Ping-API.Infrastructure/ExternalServices/DiscordWebhookClient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Ping_API.Application.Interfaces;
+using System.Text;
+using System.Text.Json;
+
+namespace Ping_API.Infrastructure.ExternalServices
+{
+    public class DiscordWebhookClient : IDiscordWebhookClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _webhookUrl;
+
+        public DiscordWebhookClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _webhookUrl = configuration["Discord:WebhookUrl"]
+                ?? throw new InvalidOperationException("Discord:WebhookUrl not configured.");
+        }
+
+        public async Task SendAsync(string message)
+        {
+            var payload = new { content = message };
+
+            var json = JsonSerializer.Serialize(payload);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(_webhookUrl, content);
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
