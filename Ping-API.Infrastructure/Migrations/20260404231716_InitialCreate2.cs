cddc9460c42758c1a39c69b2758839663f4de088
Vinícius Rodrigues Gonçalves <viniciusro741@gmail.com>
2026-04-06 08:31:16 -0300

    feat: included new migrations

---
+++ Ping-API.Infrastructure/Migrations/20260404231716_InitialCreate2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ping_API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNotified",
+                table: "Reminders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsNotified",
+                table: "Reminders");
+        }
+    }
+}
